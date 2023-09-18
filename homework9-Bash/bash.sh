a49feb342ce6aa689a350ce0bab7b1a26877e218
IvanSataev <kruto.hero.lob27@mail.ru>
2023-09-18 20:52:33 +1000

    last change

--- homework9-Bash/bash.sh
+++ homework9-Bash/bash.sh
@@ -1,3 +1,5 @@
 grep -E '^([0-9]+\.*){4}' access-4560-644067.log | cut -d' ' -f1 |  uniq -c |sort -bgr | head -n 10
 
-grep -E '^([0-9]+\.*){4}[\s|-]*\[.*]\s".+"\s\w{3}\s' access-4560-644067.log
+grep -E '^([0-9]+\.*){4}\s|-*\[.*\]\s"[GET|HEAD|POST].+"\s\w{3}\s' access-4560-644067.log | awk '{print $1 $2 $3 $4}'
+
+grep -E '^([0-9]+\.*){4}\s|-*\[.*\]\s"[GET|HEAD|POST].+"\s\w{3}\s' access-4560-644067.log | awk '{print $7}' | uniq -c |sort -bgr
