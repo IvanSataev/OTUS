e10fe24cdc198cefc2f7621de23b124615829059
IvanSataev <kruto.hero.lob27@mail.ru>
2023-10-01 21:25:12 +1000

    add new regexp

--- homework9-Bash/bash.sh
+++ homework9-Bash/bash.sh
@@ -1,5 +1,7 @@
-grep -E '^([0-9]+\.*){4}' access-4560-644067.log | cut -d' ' -f1 |  uniq -c |sort -bgr | head -n 10
+### get ip
+grep -E '^([0-9]+\.*){4}' access-4560-644067.log | cut -d' ' -f1 | sort | uniq -c |sort -bgr
 
-grep -E '^([0-9]+\.*){4}\s|-*\[.*\]\s"[GET|HEAD|POST].+"\s\w{3}\s' access-4560-644067.log | awk '{print $1 $2 $3 $4}'
-
-grep -E '^([0-9]+\.*){4}\s|-*\[.*\]\s"[GET|HEAD|POST].+"\s\w{3}\s' access-4560-644067.log | awk '{print $7}' | uniq -c |sort -bgr
+### get http
+grep -E '^([0-9]+\.*){4}(\s-?){3}\[.*\]\s"(GET|HEAD|POST).+"\s\w{3}\s' access-4560-644067.log | cut -d '"' -f 2 | cut -d ' ' -f 2| sort | uniq -c |sort -bgr
+### get code
+grep -E '^([0-9]+\.*){4}(\s-?){3}\[.*\]\s"(GET|HEAD|POST).+"\s\w{3}\s' access-4560-644067.log | cut -d '"' -f 3 | cut -d ' ' -f 2| sort | uniq -c |sort -bgr
