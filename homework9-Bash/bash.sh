e8c8ee03adbe94286495922c931d15b6211e3e7b
IvanSataev <kruto.hero.lob27@mail.ru>
2023-10-05 15:32:31 +1000

    crontab

--- homework9-Bash/bash.sh
+++ homework9-Bash/bash.sh
@@ -11,35 +11,35 @@ fi
 
 FILE_NAME=$1
 TIME_STAMP=$(date)
-FILE_NAME=access-4560-644067.log
-function get_ip() {
-    
+
+function get_ip { 
 grep -E '^([0-9]+\.*){4}' ${FILE_NAME} \
-| cut -d' ' -f1 \
+| cut -d ' ' -f1 \
 | sort \
 | uniq -c \
 | sort -bgr \
-| awk 'FS=" " { print "Number of request: " $1, "IP: "$2 }' | head -n 10
+| awk '{ print "Number of request: " $1, "IP: " $2 }' | head -n 10
 }
+
 function get_http_request {
 grep -E '^([0-9]+\.*){4}(\s-?){3}\[.*\]\s"(GET|HEAD|POST).+"\s\w{3}\s' ${FILE_NAME} \
 | cut -d '"' -f 2 \
 | cut -d ' ' -f 2 \
 | sort | uniq -c \
 |sort -bgr \
-| awk '{ print "Number of request: " $1, "URL: "$2 }' | head -n 10
+| awk '{ print "Number of request: " $1, "URL: " $2 }' | head -n 10
 }
 
 function get_error_code {
 grep -E '^([0-9]+\.*){4}(\s-?){3}\[.*\]\s"(GET|HEAD|POST).+"\s\w{3}\s' ${FILE_NAME} \
 | cut -d '"' -f 3 | cut -d ' ' -f 2 \
 | sort | uniq -c |sort -bgr \
-| awk '{ print "Number of request: " $1, "CODE: "$2 }'
+| awk '{ print "Number of request: " $1, "CODE: " $2 }'
 }
 
 $(mutt -v &>/dev/null)
 
-if [ ! $? -eq 0 ] 
+if [  $? -ne 0 ] 
 then
     echo "Error: mutt not installed"
     exit 1
@@ -47,9 +47,9 @@ fi
 
 if [ ! -N ${FILE_NAME} ]  
 then
-$(truncate -s 0 test)
 
-cat >> test << EOF 
+
+mutt -s "subject" -- sataev.i@samberi.com << EOF 
 Data for the period:$TIME_STAMP
 URL:
 $(get_http_request)
@@ -60,7 +60,5 @@ $(get_ip)
 Error code:
 $(get_error_code)
 EOF
-
-cat test | mutt -s "subject" -- sataev.i@samberi.com
 fi
- 
+#* * * * * root flock -w0 /tmp/1.lock /bash.sh 
