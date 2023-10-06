70302d1dc30633d186de3a25789b589a1afcf354
IvanSataev <kruto.hero.lob27@mail.ru>
2023-10-06 17:43:51 +1000

    grafana config

--- homework14-Prometheus/prometheus.sh
+++ homework14-Prometheus/prometheus.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-yum install vim docker git wget gcc openssl-devel readline-devel systemd-devel make pcre-devel -y
+sudo -i
+yum install vim docker git wget  -y
+setenforce 0
 systemctl start docker
 cat >> prometheus.yml << EOF
 global:
@@ -16,13 +17,14 @@ scrape_configs:
   - job_name: node
     static_configs:
       - targets: ['10.0.2.15:9100']
+remote_write:
+- url: http://10.0.2.15:3000
+  basic_auth:
+    username: admin
+    password: test
+      
 EOF
 
-docker run -d -p  9090:9090 -v prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
-wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz  --no-check-certificate
-tar xvfz node_exporter-1.6.1.linux-amd64.tar.gz
-
-cp node_exporter-1.6.1.linux-amd64/node_exporter /usr/sbin/node_exporter
 cat >>  /etc/systemd/system/node_exporter.service << EOF
 [Unit]
 Description=Node Exporter
@@ -35,5 +37,14 @@ Restart=Always
 WantedBy=default.target
 EOF
 
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.6.1/node_exporter-1.6.1.linux-amd64.tar.gz  --no-check-certificate
+tar xvfz node_exporter-1.6.1.linux-amd64.tar.gz
+
+cp node_exporter-1.6.1.linux-amd64/node_exporter /usr/sbin/node_exporter
+
 systemctl enable node_exporter.service
 systemctl start node_exporter.service
+
+docker run -d -p 3000:3000 --name=grafana grafana/grafana-enterprise
+docker run -d -p  9090:9090 -v /root/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
