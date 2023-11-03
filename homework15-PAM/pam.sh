31c9461c3ca02a2f1587fa7703281d8c54c14500
IvanSataev <kruto.hero.lob27@mail.ru>
2023-11-03 23:32:14 +1000

    fix homework15

--- homework15-PAM/pam.sh
+++ homework15-PAM/pam.sh
@@ -1,8 +1,39 @@
-sduo -i
+sudo -i
+### login by password
 sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd.service
-sudo useradd otusadm && sudo useradd otus
+### create test user
+useradd otusadm
+useradd otus
+echo "test" |  passwd --stdin otusadm && echo "test" | passwd --stdin otus
 
-echo "Otus2022!" | sudo passwd --stdin otusadm && echo "Otus2022!" | sudo passwd --stdin otus
 groupadd -f admin
- 
+### adding to group admin 
+usermod otusadm -a -G admin
+usermod root -a -G admin 
+usermod vagrant -a -G admin
+### script for limitation
+cat >> /usr/local/bin/login.sh << EOF
+#!/bin/bash
+#Первое условие: если день недели суббота или воскресенье
+if [ \$(date +%a) = "Sat" ] || [ \$(date +%a) = "Sun" ]; then
+ #Второе условие: входит ли пользователь в группу admin
+ if \$(getent group admin | grep -qw "\$PAM_USER"); then
+    #Если пользователь входит в группу admin, то он может подключиться
+     exit 0
+ else
+    #Иначе ошибка (не сможет подключиться)
+    exit 1
+ fi
+  #Если день не выходной, то подключиться может любой пользователь
+else
+    exit 0
+fi
+
+EOF
+
+chmod +x /usr/local/bin/login.sh
+### set saturday
+date 11042023
+### adding pam config
+sed -i 's/pam_nologin.so/pam_nologin.so\n account    required  pam_exec.so \/usr\/local\/bin\/login.sh/g' /etc/pam.d/sshd
+systemctl restart sshd.service
