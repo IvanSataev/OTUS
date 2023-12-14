a0677e16d20633ab5fb02e6a8fab7e65c3321075
IvanSataev <kruto.hero.lob27@mail.ru>
2023-12-14 19:27:29 +1000

    init homework 20

--- homework10-Process/lsof.sh
+++ homework10-Process/lsof.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #find  /proc/ -type d  -maxdepth 1 |grep -Eo '[0-9]+'
  for x in `ps -eF| awk '{ print $2 }'`; \
- do echo `ls /proc/$x/fd 2> /dev/null | \
- wc -l` $x `cat /proc/$x/cmdline 2> /dev/null`; \
+ do echo `ls /proc/$x/fd 2> /dev/null |  wc -l` $x `cat /proc/$x/cmdline 2> /dev/null`; \
  done | sort -n -r | head -n 20
