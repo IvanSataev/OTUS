75dd7f2d73b5444b31ab5094cd6007d5014b1605
IvanSataev <kruto.hero.lob27@mail.ru>
2024-01-12 19:35:02 +1000

    lsof

--- homework10-Process/lsof.sh
+++ homework10-Process/lsof.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-#find  /proc/ -type d  -maxdepth 1 |grep -Eo '[0-9]+'
- for x in `ps -eF| awk '{ print $2 }'`; \
- do echo `ls /proc/$x/fd 2> /dev/null |  wc -l` $x `cat /proc/$x/cmdline 2> /dev/null`; \
- done | sort -n -r | head -n 20
+format="%-10s %-20s %-30s %15s\n" 
+
+printf "$format" PID USER NAME COMM
+
+ for proc in `ps -eF | awk '{ print $2 }'`
+
+ do 
+    test="/proc/$proc"
+   if [[ -d "$test" ]]
+    then
+    user=`awk '/Uid/{print $2}' /proc/$proc/status`
+    comm=`cat /proc/$proc/comm`
+    if [[ user -eq 0 ]]
+     then
+       user_name='root'
+     else
+       #user_name=`grep $user /etc/passwd | awk -F ":" '{print $1}'`
+       user_name=`id $user | awk '{print $1}' | awk -F "(" '{print $2}' | awk -F ")" '{print $1}'`
+       echo $user_name
+    fi
+    map_files=`readlink /proc/$proc/map_files/*; readlink /proc/$proc/cwd`
+    if ! [[ -z "$map_files" ]]
+    then
+    for num in $map_files
+    do
+    printf "$format" $proc $user_name $num $comm
+    done
+    fi
+   fi 
+ done 
+
+ 
